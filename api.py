1f673751d4e0dc0345a60b14166c74344f9a929b
Tolga Sümer <tolga.sumer@tedu.edu.tr>
2019-07-03 23:55:16 +0300

    init

---
+++ api.py
@@ -0,0 +1,68 @@
+from flask import Flask, send_file
+from flask_restful import Resource, Api, reqparse
+from face_recognition_knn import predict, show_prediction_labels_on_image
+from io import BytesIO
+from PIL import Image
+import werkzeug
+import os
+
+app = Flask(__name__)
+api = Api(app)
+UPLOAD_FOLDER = 'uploads'
+RESULT_FOLDER = 'results'
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'file', type=werkzeug.datastructures.FileStorage, location='files')
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+## util for serving a PIL image object
+def serve_pil_image(pil_img):
+    cached_img = BytesIO() # caching the file
+    pil_img.save(cached_img, 'JPEG', quality=70)
+    cached_img.seek(0)
+    return send_file(cached_img, mimetype='image/jpeg')
+
+# util that takes a normal FileStorage image file and resizes it
+def resize_image(img_file):
+    pil_img = Image.open(img_file) # turn the image file into a pil image
+    pil_img.thumbnail((1000,1000),Image.ANTIALIAS) # resize the image while keeping the aspect ratio, max=500,500
+    cached_img = BytesIO() # caching the file
+    pil_img.save(cached_img, 'JPEG', quality=70) # saving the pil image into an imagefile
+    return cached_img
+
+
+class PhotoUpload(Resource):
+    decorators = []
+
+    def post(self):
+        data = parser.parse_args()
+        if data['file'] == "":
+            return {
+                'data': '',
+                'message': 'No file found',
+                'status': 'error'
+            }
+        photo = data['file']
+
+        if photo:
+            print(photo)
+            resized_photo = resize_image(photo)
+            predictions = predict(resized_photo, model_path="trained_knn_model.clf")
+            result_img = show_prediction_labels_on_image(resized_photo, predictions)
+            return serve_pil_image(result_img)
+        return {
+            'data': '',
+            'message': 'Something when wrong',
+            'status': 'error'
+        }
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(PhotoUpload, '/upload')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ face_recognition_knn.py
@@ -0,0 +1,119 @@
+"""
+This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
+
+When should I use this example?
+This example is useful when you wish to recognize a large set of known people,
+and make a prediction for an unknown person in a feasible computation time.
+
+Algorithm Description:
+The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
+in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
+in its training set, and performing a majority vote (possibly weighted) on their label.
+
+For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
+and two images of Obama, The result would be 'Obama'.
+
+* This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
+
+Usage:
+
+1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
+   with a sub-directory for each known person.
+
+2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
+   want to save the model to disk so you can re-use the model without having to re-train it.
+
+3. Call 'predict' and pass in your trained model to recognize the people in an unknown image.
+
+NOTE: This example requires scikit-learn to be installed! You can install it with pip:
+
+$ pip3 install scikit-learn
+
+"""
+
+import math
+from sklearn import neighbors
+import os
+import os.path
+import pickle
+from PIL import Image, ImageDraw
+import face_recognition
+from face_recognition.face_recognition_cli import image_files_in_folder
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+
+
+def predict(image, knn_clf=None, model_path=None, distance_threshold=0.6):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+
+    :param X_img_path: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+    X_img = face_recognition.load_image_file(image)
+    X_face_locations = face_recognition.face_locations(X_img)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+def show_prediction_labels_on_image(image, predictions):
+    """
+    Shows the face recognition results visually.
+
+    :param img_path: path to image to be recognized
+    :param predictions: results of the predict function
+    :return:
+    """
+    pil_image = Image.open(image).convert("RGB")
+    draw = ImageDraw.Draw(pil_image)
+
+    for name, (top, right, bottom, left) in predictions:
+        # Draw a box around the face using the Pillow module
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+        # There's a bug in Pillow where it blows up with non-UTF-8 text
+        # when using the default bitmap font
+        name = name.encode("UTF-8")
+
+        # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+    # Remove the drawing library from memory as per the Pillow docs
+    del draw
+
+    # Return the resulting image
+    return pil_image
+
+
+#if __name__ == "__main__":
