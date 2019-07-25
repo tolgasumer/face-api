c11fb9045136596dec4f48d82cdf622bd50aab11
Tolga Sümer <tolga.sumer@tedu.edu.tr>
2019-07-25 04:03:10 +0300

    Update face_recognition_knn.py

--- face_recognition_knn.py
+++ face_recognition_knn.py
@@ -1,10 +1,4 @@
 """
-This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
-
-When should I use this example?
-This example is useful when you wish to recognize a large set of known people,
-and make a prediction for an unknown person in a feasible computation time.
-
 Algorithm Description:
 The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
 in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
@@ -15,19 +9,9 @@ and two images of Obama, The result would be 'Obama'.
 
 * This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
 
-Usage:
-
-1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
-   with a sub-directory for each known person.
-
-2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
-   want to save the model to disk so you can re-use the model without having to re-train it.
-
-3. Call 'predict' and pass in your trained model to recognize the people in an unknown image.
-
-NOTE: This example requires scikit-learn to be installed! You can install it with pip:
-
-$ pip3 install scikit-learn
+*** NOTE: This is the original face_recognition_knn example from the face_recognition library, 
+slightly modified to work with base64 images that are parsed from flask endpoints. You can use this approach
+to modify any other function from the original face_recognition library to make it work with an active stateless flask backend.
 
 """
 
